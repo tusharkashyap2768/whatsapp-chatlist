688126305c034c24b3068ff692a9bba3faac1f9a
Tushar kashyap <75391307+tusharkashyap2768@users.noreply.github.com>
2022-05-27 15:56:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ import { ChatHandler, chat_names } from './ChatHandler.js';
 
 onload = function () {
 
-    const chatlist = document.getElementById('chat-list');
-    const add = document.getElementById('generate-step');
-    const text = document.getElementById('temptext');
+    const chatlist = document.getElementById('chat-list');  // chatlist
+    const add = document.getElementById('generate-step'); //generate button
+    const text = document.getElementById('temptext');  //refrance to the text area 
 
-    const templates = document.getElementsByTagName('template')[0];
-    const chat_item = templates.content.querySelector("li");
+    const templates = document.getElementsByTagName('template')[0]; // referenace to the list items that were in the template
+    const chat_item = templates.content.querySelector("li"); //list item
 
     const chatHandler = new ChatHandler(chat_item, chatlist);
-    let chats = [];
+    let chats = []; // id of sabi logo ka jo chat list mai hai 
 
     add.onclick = function () {
+        
+         // agar r> 0.75 se bada aur chat empty nhi hai
         if(Math.random()>0.75 && chats.length > 0){
             let index = Math.floor(Math.random()*chats.length);
+            
+            //random id lo aur delete krdo aur fir update krdo
             let idToDelete = chats[index];
             chatHandler.deleteMsg(idToDelete);
             text.innerHTML = "Deleted message from "+chat_names[idToDelete] + "<br>" + text.innerHTML;
@@ -26,6 +30,7 @@ onload = function () {
                 chats.push(idOfMsg);
             }
             chatHandler.newMsg(idOfMsg);
+            //text area m btao msg aya
             text.innerHTML = "New message from "+chat_names[idOfMsg] + "<br>" + text.innerHTML;
         }
     };
